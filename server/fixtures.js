c8af58526fadadc02ad6900bc7d8284add0fb0ec
vladbalan <vlad.balan.dev@gmail.com>
2015-03-05 12:43:49 +0200

    New fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,19 @@
 // Fixture data 
+function rand(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randArray(array) {
+  return array[rand(array.length)];
+}
+
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
   var words = [
     ["Big stupid", "Very basic", "Awesome", "Cool", "Easy", "DAE hate", "Me and my", "Bad", "The legend of", "Ask me about my", "About the", "Looking for my", "The definition of", "In love with the", "Damn it feels good to be"],
     ["car", 'ship', 'plane', 'cat', 'dog', 'sword', 'droid', 'chair', 'dream', 'skill', 'awesomenes', 'game', 'dragon', 'gangsta'],
-    ["s", "", "", "", ""]
+    ["s", "", "", "", ""],
+    [".", ".", ".", ",", ",", ",", ",", ";", "!", "!", "?"]
   ]
 
   var jonId = Meteor.users.insert({
@@ -13,16 +22,35 @@ if (Posts.find().count() === 0) {
   var jon = Meteor.users.findOne(jonId);
 
   for (var i = 0; i < 50; i++) {
-    var noun = words[1][Math.floor(Math.random()*words[1].length)];
-    Posts.insert({
-      title:  words[0][Math.floor(Math.random()*words[0].length)] + ' ' +
-              noun + words[2][Math.floor(Math.random()*words[2].length)],
+    var noun = randArray(words[1]);
+    var commentsCount = rand(10);
+    var postId = Posts.insert({
+      title:  randArray(words[0]) + ' ' +
+              noun + randArray(words[2]),
       author: jon.profile.name,
       userId: jon._id,
       url: 'http://gamiphy.meteor.com/links/' + noun + '-' + i,
       submitted: now - i * 3600 * 1000,
-      commentsCount: 0,
+      commentsCount: commentsCount,
       upvoters: [], votes: 0
     });
+
+    for (var j = 0; j < commentsCount; j++) {
+      var currentBody = '';
+      for (var k = 1; k < rand(10); k++) {
+        currentBody +=  randArray(words[0]) + ' ' +
+                        randArray(words[1]) + 
+                        randArray(words[2]) + 
+                        randArray(words[3]) + ' ';
+      }
+      var dateFactor = (j > i) ? i : j;
+      Comments.insert({
+        userId: jon._id,
+        author: jon.profile.name,
+        postId: postId,
+        submitted: now - dateFactor * 3600 * 1000,
+        body: currentBody
+      })
+    }
   }
 }
