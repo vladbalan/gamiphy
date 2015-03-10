525863828fa9bf009a8312fcb238afa62305a6d5
vladbalan <vlad.balan.dev@gmail.com>
2015-03-10 18:05:37 +0200

    Change frontend layout

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -6,3 +6,14 @@ Handlebars.registerHelper('pluralize', function(n, thing) {
 	return n + ' ' + thing + 's';
   }
 });
+
+Handlebars.registerHelper('activeRouteClass', function(/* route names */) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.pop();
+    
+    var active = _.any(args, function(name) {
+      return Router.current() && Router.current().route.getName() === name
+    });
+    
+    return active && 'active';
+});
